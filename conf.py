7bca960c207f675ba9bcc3dc9c49955c61ff7110
jmcalalang <jon.calalang@gmail.com>
2020-12-17 17:40:42 +0000

    path

--- conf.py
+++ conf.py
@@ -17,7 +17,7 @@ import re
 import pkgutil
 import string
 import f5_sphinx_theme
-sys.path.insert(0, os.path.abspath('../..'))
+sys.path.insert(0, os.path.abspath('docs/_themes/'))
 
 # -- Project information -----------------------------------------------------
 
