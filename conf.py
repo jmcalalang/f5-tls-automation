1a25ac5941c920cfd7dbd68df4164f6543adfb53
jmcalalang <jon.calalang@gmail.com>
2020-12-18 17:44:34 +0000

    so lost

--- conf.py
+++ conf.py
@@ -73,9 +73,10 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # a list of builtin themes.
 #
 # html_theme = 'alabaster'
-
-html_theme_path = ["docs/_themes/"]
+# import f5_sphinx_theme
 html_theme = 'f5_sphinx_theme'
+html_theme_path = ["docs/_themes/"]
+
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
